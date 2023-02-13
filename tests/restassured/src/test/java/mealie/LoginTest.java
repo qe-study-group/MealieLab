db59869f88776a4b2e19d337240e6c6e5eb138c2
David Vydra <david.vydra@sap.com>
2023-02-12 17:55:25 -0800

    WIP

--- tests/restassured/src/test/java/mealie/LoginTest.java
+++ tests/restassured/src/test/java/mealie/LoginTest.java
@@ -1,19 +1,9 @@
 package mealie;
 
 import io.restassured.RestAssured;
-import io.restassured.builder.RequestSpecBuilder;
-import io.restassured.config.HeaderConfig;
-import io.restassured.config.RestAssuredConfig;
-import io.restassured.filter.Filter;
-import io.restassured.filter.FilterContext;
-import io.restassured.http.ContentType;
 
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 
-import java.util.List;
-
-import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.contains;
 import static org.hamcrest.Matchers.equalTo;
 
 public class LoginTest {
